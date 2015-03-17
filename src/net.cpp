74dc164e349ee72823f5bec31d7c5efc67ab919f
Adaryian Alexander <Adaryian@eCurrency.io>
2015-03-17 19:43:40 -0400

    Hard Fork Updates
    Fixes for Hard Fork

--- src/net.cpp
+++ src/net.cpp
@@ -1191,13 +1191,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"bluematt.me", "coingen-seed.bluematt.me"},
-    {NULL, NULL}
+    {"mine.ecurrency.io", "mine.ecurrency.io"},
+	{NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"bitcoin.petertodd.org", "testnet-seed.ecurrency.petertodd.org"},
-    {"bluematt.me", "testnet-seed.bluematt.me"},
+    {"mine.ecurrency.io", "mine.ecurrency.io"},
     {NULL, NULL}
 };
 
