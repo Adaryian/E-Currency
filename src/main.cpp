f176f6aedc018604f8337092a036ddade1cd8f68
Adaryian Alexander <Adaryian@eCurrency.io>
2015-03-21 23:06:29 -0400

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -3238,8 +3238,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         }
 		{
 		if (((pfrom->nVersion < 70002) && ((pindexBest->nHeight) > 32256)))
-	{
-	//disconnect from older peers -> for the fork.
+		//disconnect from older peers -> for the fork.
 	printf("partner %s using obsolete version %i; disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->nVersion);
 	pfrom->fDisconnect = true;
         return false;
